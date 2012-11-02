5765cf8ce0c2d62596efe1f3e951c0e103e250a8
Marcelo G. Silva <mgswolf@gmail.com>
2012-11-02 18:44:27 -0200

    exercicies cap 10

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -35,5 +35,33 @@ describe "Micropost pages" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
     end
+
+    describe "as a wrong user" do
+      let(:wrong_user) { FactoryGirl.create(:user) }
+      before do
+        signin wrong_user
+        visit user_path(user)
+      end
+
+      it { should_not have_content('delete')}
+    end
+  end
+
+  describe "sidebar microposts count" do
+    before  do
+      FactoryGirl.create(:micropost, user: user)
+      visit user_path(user)
+    end
+
+    it { should have_content("Micropost (1)")}
+
+    describe "pluralize for many microposts" do
+      before do
+        3.times { FactoryGirl.create(:micropost, user: user ) }
+        visit user_path(user)
+      end
+
+      it { should have_content("Microposts (4)") }
+    end
   end
 end
